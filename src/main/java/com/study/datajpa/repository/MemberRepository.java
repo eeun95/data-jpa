c6c98661632ea4f9c55c00db31ffad0245dc0ca8
eeun95 <eeun95@gmail.com>
2023-04-06 14:18:46 +0900

    쿼리 메서드 기능 - @EntityGraph

--- src/main/java/com/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/study/datajpa/repository/MemberRepository.java
@@ -4,7 +4,7 @@ import com.study.datajpa.dto.MemberDto;
 import com.study.datajpa.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -70,5 +70,23 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying(clearAutomatically = true)
     @Query("update Member m set m.age = m.age+1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    // Member를 조회할 때 연관된 Team도 함께 조회
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    // 내부적으론 페치 조인을 쓰는 것, JPQL없이도 연관관계를 엮어서 가지고 올 수 있음
+    @EntityGraph(attributePaths = "team")
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = "team")
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    //@EntityGraph(attributePaths = {"team"})
+    // JPA 표준 스펙 2.2에 들어가는 내용 @EntityGraph
+    @EntityGraph("Member.all")
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
 
 }
