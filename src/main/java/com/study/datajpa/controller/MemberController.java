c25de88b4ad62131f1de9b0d9021092b3dffa6f9
eeun95 <eeun95@gmail.com>
2023-04-08 20:06:47 +0900

    확장기능 - 도메인 클래스 컨버터

---
+++ src/main/java/com/study/datajpa/controller/MemberController.java
@@ -0,0 +1,34 @@
+package com.study.datajpa.controller;
+
+import com.study.datajpa.entity.Member;
+import com.study.datajpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        // 스프링부트가 자동으로 컨버터 해서 Member 객체를 반환해준다
+        // 조회용으로만 활용해야 한다. 트랜잭션이 없는 범위에서 조회했으므로 엔티티를 변경해도 DB반영 X
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("UserA"));
+    }
+}
